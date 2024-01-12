5f25ca0ed37e13fd79fa8fbedca79d2e3684d864
tireks <33616425+tireks@users.noreply.github.com>
2024-01-12 17:54:48 +0700

    12

---
+++ 932103.leonov.ruslan.lab12/leonov_lab12/leonov_lab12/Controllers/Calc.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using leonov_lab12.Models;
+
+namespace leonov_lab12.Controllers
+{
+    public class Calc : Controller
+    {
+        public IActionResult Manual()
+        {
+            if (Request.Method == "POST")
+            {
+                Actions actions = new Actions();
+                actions.value1 = Request.Form["value1"];
+                actions.value2 = Request.Form["value2"];
+                actions.operation = Request.Form["operation"];
+                int x;
+                if (int.TryParse(actions.value1, out x) && int.TryParse(actions.value2, out x))
+                    ViewBag.Result = actions.getResult();
+                else ViewBag.Result = "Nulls Components Exists";
+                return View("Result");
+            }
+            else
+                return View();
+        }
+        [HttpGet]
+        public IActionResult ManualWithSeparateHandlers()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ManualWithSeparateHandlers(string str)
+        {
+            Actions actions = new Actions();
+            actions.value1 = Request.Form["value1"];
+            actions.value2 = Request.Form["value2"];
+            actions.operation = Request.Form["operation"];
+            int x;
+            if (int.TryParse(actions.value1, out x) && int.TryParse(actions.value2, out x))
+                ViewBag.Result = actions.getResult();
+            else ViewBag.Result = "Nulls Components Exists";
+            return View("Result");
+        }
+        [HttpPost]
+        public IActionResult ModelBindingInParameters(string val1, string val2, string oper)
+        {
+            Actions actions = new Actions();
+            actions.value1 = val1;
+            actions.value2 = val2;
+            actions.operation = oper;
+            int x;
+            if (int.TryParse(actions.value1, out x) && int.TryParse(actions.value2, out x))
+                ViewBag.Result = actions.getResultModel();
+            else ViewBag.Result = "Nulls Components Exists";
+            return View("Result");
+        }
+        [HttpGet]
+        public IActionResult ModelBindingInParameters()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ModelBindingInSeparateModel(Actions actions)
+        {
+            int x;
+            if (int.TryParse(actions.value1, out x) && int.TryParse(actions.value2, out x))
+                ViewBag.Result = actions.getResultModel();
+            else ViewBag.Result = "Nulls Components Exists";
+            return View("Result");
+        }
+        [HttpGet]
+        public IActionResult ModelBindingInSeparateModel()
+        {
+            return View();
+        }
+    }
+}
---
+++ 932103.leonov.ruslan.lab12/leonov_lab12/leonov_lab12/Models/Actions.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace leonov_lab12.Models
+{
+    public class Actions
+    {
+        public string value1 { get; set; }
+        public string value2 { get; set; }
+        public string operation { get; set; }
+
+        public string getResult()
+        {
+            int a = Int32.Parse(value1);
+            int b = Int32.Parse(value2);
+            switch ((int)Convert.ToChar(operation))
+            {
+                case 42: return (value1) + " * " + (value2) + " = " + (a * b);
+                case 43: return (value1) + " + " + (value2) + " = " + (a + b);
+                case 45: return (value1) + " - " + (value2) + " = " + (a - b);
+                case 47:
+                    try
+                    {
+                        return (value1) + " / " + (value2) + " = " + (a / b);
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        return "Division by zero!";
+                    }
+
+            }
+            return "b";
+        }
+        public string getResultModel()
+        {
+            int a = Int32.Parse(value1);
+            int b = Int32.Parse(value2);
+            int c;
+            switch ((int)Convert.ToChar(operation))
+            {
+                case 42:
+                    c = a * b;
+                    return c.ToString();
+                case 43:
+                    c = a + b;
+                    return c.ToString();
+                case 45:
+                    c = a - b;
+                    return c.ToString();
+                case 47:
+                    try
+                    {
+                        c = a / b;
+                        return c.ToString();
+                    }
+                    catch (DivideByZeroException)
+                    {
+                        return "Division by zero!";
+                    }
+            }
+            return "b";
+        }
+    }
+}
